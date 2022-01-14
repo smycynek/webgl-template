01fb855e7e09f29dadf9d3471aaa85ccb25f528d
smycynek <smycynek@gmail.com>
2022-01-14 14:31:11 -0500

    add version info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
+import { Version } from './version';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, Version
   ],
   imports: [
     BrowserModule, FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, Version]
 })
 export class AppModule { }
 
---
+++ src/app/version.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'version',
+  templateUrl: './version.html',
+  styles: []
+})
+export class Version {
+}
